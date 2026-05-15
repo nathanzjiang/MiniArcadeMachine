ac1ae0cda1fe184f934cfc226c3ffeeb7ce7d32d
changsu <changsu@xosphere.io>
2026-05-14 23:45:31 -0700

    Draw several assets

--- src/assets/MarioAssets.h
+++ src/assets/MarioAssets.h
@@ -37,43 +37,40 @@ struct Sprite16 {
 constexpr uint16_t SKY = hexToRgb565(0x7DFFFF);
 constexpr uint16_t BLACK = hexToRgb565(0x000000);
 constexpr uint16_t WHITE = hexToRgb565(0xFFFFFF);
-constexpr uint16_t GROUND_LIGHT = hexToRgb565(0xC58E31);
-constexpr uint16_t GROUND_MID = hexToRgb565(0xA06020);
-constexpr uint16_t GROUND_DARK = hexToRgb565(0x603600);
+constexpr uint16_t GROUND_TAN = hexToRgb565(0xFEBE81);
+constexpr uint16_t GROUND_RED = hexToRgb565(0xE55000);
 constexpr uint16_t COIN_YELLOW = hexToRgb565(0xFFE600);
 constexpr uint16_t COIN_ORANGE = hexToRgb565(0xFF9E00);
 constexpr uint16_t PLAYER_RED = hexToRgb565(0xB92716);
 constexpr uint16_t PLAYER_SKIN = hexToRgb565(0xF0AC3F);
 constexpr uint16_t PLAYER_GREEN = hexToRgb565(0x736702);
 constexpr uint16_t UNKNOWN_COLOR = hexToRgb565(0xFF00FF);
-constexpr uint16_t GOOMBA_BROWN = hexToRgb565(0xA14528);
-constexpr uint16_t GOOMBA_TAN = hexToRgb565(0xD6A552);
+constexpr uint16_t GOOMBA_RED = hexToRgb565(0xE65100);
+constexpr uint16_t GOOMBA_TAN = hexToRgb565(0xFFCD94);
 
 static const PaletteEntry GROUND_PALETTE[] = {
-    {'.', GROUND_DARK},
-    {'l', GROUND_LIGHT},
-    {'m', GROUND_MID},
-    {'d', GROUND_DARK},
-    {'k', BLACK}
+    {'r', GROUND_RED},
+    {'t', GROUND_TAN},
+    {'b', BLACK}
 };
 
 static const char *const GROUND_ROWS[] = {
-    "dddddddddddddddd",
-    "dlllllllllllllld",
-    "dlmmmmmmmmmmmldd",
-    "dlmllllmllllmldd",
-    "dlmlkkkmlkkkmldd",
-    "dlmllllmllllmldd",
-    "dlmmmmmmmmmmmldd",
-    "dddddddddddddddd",
-    "dlllllllllllllld",
-    "dlmmmmmmmmmmmldd",
-    "dlmllllmllllmldd",
-    "dlmlkkkmlkkkmldd",
-    "dlmllllmllllmldd",
-    "dlmmmmmmmmmmmldd",
-    "dlllllllllllllld",
-    "dddddddddddddddd"
+    "rttttttttbrttttr",
+    "brrrrrrrrbtrrrrt",
+    "brrrrrrrrbtrrrrt",
+    "brrrrrrrrbtrrrrt",
+    "brrrrrrrrbtbrrrt",
+    "brrrrrrrrbrbbbbr",
+    "brrrrrrrrbtttttb",
+    "brrrrrrrrbtrrrrb",
+    "brrrrrrrrbtrrrrb",
+    "brrrrrrrrbtrrrrb",
+    "bbrrrrrrbtrrrrrb",
+    "ttbbrrrrbtrrrrrb",
+    "trttbbbbtrrrrrrb",
+    "trrrtttbtrrrrrrb",
+    "trrrrrrbtrrrrrbb",
+    "rbbbbbbrbbbbbbbr"
 };
 
 static const PaletteEntry COIN_PALETTE[] = {
@@ -152,29 +149,29 @@ static const char *const PLAYER_JUMP_ROWS[] = {
 
 static const PaletteEntry GOOMBA_PALETTE[] = {
     {'.', TRANSPARENT},
-    {'b', GOOMBA_BROWN},
+    {'r', GOOMBA_RED},
     {'t', GOOMBA_TAN},
-    {'k', BLACK},
+    {'b', BLACK},
     {'w', WHITE}
 };
 
 static const char *const GOOMBA_ROWS[] = {
-    "................",
-    "................",
-    ".....bbbbbb.....",
-    "...bbbbbbbbbb...",
-    "..bbbbbbbbbbbb..",
-    ".bbbbbbbbbbbbbb.",
-    ".bbbkkbbbbkkbbb.",
-    "bbbbkwbbbbwkbbbb",
-    "bbbbkkkkkkkkbbbb",
-    ".bbbbbbbbbbbbbb.",
+    "......rrrr......",
+    ".....rrrrrr.....",
+    "....rrrrrrrr....",
+    "...rrrrrrrrrr...",
+    "..rbbrrrrrrbbr..",
+    ".rrrwbrrrrbwrrr.",
+    ".rrrwbbbbbbwrrr.",
+    "rrrrwbwrrwbwrrrr",
+    "rrrrwwwrrwwwrrrr",
+    "rrrrrrrrrrrrrrrr",
+    ".rrrrttttttrrrr.",
+    "....tttttttt....",
     "..bbttttttttbb..",
-    "...tttttttttt...",
-    "..kkk......kkk..",
-    ".kkkk......kkkk.",
-    "..kkk....kkk....",
-    ".kkkk....kkkk..."
+    ".bbbbbttttbbbbb.",
+    ".bbbbbbttbbbbbb.",
+    "..bbbbb..bbbbb.."
 };
 
 static const Sprite16 GROUND = {GROUND_PALETTE, 5, GROUND_ROWS, false};
